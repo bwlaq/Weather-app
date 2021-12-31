1a06ccf4c7546f676131af948c55b1769729c5e9
Jeng-han Lu <e50639@ebusa.com>
2021-12-31 16:14:32 +1000

    weather app init

---
+++ src/components/WeatherCard.svelte
@@ -0,0 +1,16 @@
+<script>
+    let location = 'Brisbane';
+</script>
+
+<style>
+    #weather-card{
+        background-color: white;
+    }
+</style>
+
+<h1>
+    Weather in {location}
+</h1>
+<div id = 'weather-card'>
+    
+</div>
