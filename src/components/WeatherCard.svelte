909d3c8fba5f8102680fc3fde7587a44f972d082
Jeng-han Lu <e50639@ebusa.com>
2022-01-04 11:05:10 +1000

    unable to fetch BOM data

--- src/components/WeatherCard.svelte
+++ src/components/WeatherCard.svelte
@@ -1,5 +1,9 @@
 <script>
     let location = 'Brisbane';
+    const fetchWeather = ( async ()=> {
+        const response = await fetch ('http://www.bom.gov.au/fwo/IDQ60801/IDQ60801.94576.json', {mode: 'no-cors'})
+        return await response.json();
+    })()
 </script>
 
 <style>
@@ -11,6 +15,14 @@
 <h1>
     Weather in {location}
 </h1>
+
 <div id = 'weather-card'>
-    
+    {#await fetchWeather}
+    <p>...fetching weather</p>
+    {:then data}
+    <h3>{data.observations}</h3>
+    <p></p>
+    {:catch error}
+	<p>{error}</p>
+    {/await}
 </div>
